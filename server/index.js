321582082729f5dc50e2e85dff9a0480635c2f69
lnuvy <hangeulgod@naver.com>
2021-12-17 03:51:04 +0900

    cors 세팅, request body 파싱

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
-var express = require('express')
+var express = require('express') // express 관련 모듈 불러오기
 var app = express()
+var cors = require('cors') // cors 설정
 
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
+app.use(express.json()) // 파싱
 
 app.get('/hello', (req, res) => {
   res.send('hello world!')
+})
+
+app.use( (req, res, next) => {
+  res.status(404).send('Sorry 404')
+})
+
+app.use( (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("broken on server")
 })
 
 app.listen(5000, () => {
