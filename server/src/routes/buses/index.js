1751424608b23c299be5bc9d2efd9708bfff4b04
lnuvy <hangeulgod@naver.com>
2021-12-22 12:24:04 +0900

    윈도우에서 정상작동 확인

--- server/src/routes/buses/index.js
+++ server/src/routes/buses/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const DataRouter = express.Router()
 const Bues = require('../../models/Buse')
 
-DataRouter.route('/').get( async (req, res) => {
+DataRouter.route('/').get(async (req, res) => {
   const buses = await Bues.find()
   console.log(buses);
   res.json({ status: 200, buses })
